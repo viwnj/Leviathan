c6f6a7ce974514ce89fd76f1365101eebaf863c5
Vitor Henrique <35904476+viwnj@users.noreply.github.com>
2020-03-21 20:51:13 -0300

    Chore: Docs

--- Sandbox/src/Collision.cpp
+++ Sandbox/src/Collision.cpp
@@ -1,5 +1,7 @@
 #include "Collision.h"
 #include <iostream>
+
+// https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 bool Collision::CheckRectCollision(const SDL_Rect& rectA, const SDL_Rect& rectB){
 	return (
 		rectA.x + rectA.w >= rectB.x &&
